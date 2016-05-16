025d0d96053a3284474bbb11c6dbcbd3ec7d7237
Liz Favre <egfavre@gmail.com>
2016-05-16 11:52:15 -0400

    initial import day 1 during class

---
+++ src/Person.java
@@ -0,0 +1,10 @@
+/**
+ * Created by user on 5/16/16.
+ */
+public class Person {
+    String name;
+    int age;
+    boolean isAlive;
+    //create Person Array in Person class- self referntial
+    Person[] family = new Person[10];
+}
