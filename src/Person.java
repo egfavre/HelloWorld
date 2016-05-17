36a335e2bae85912f145e16120307993960e1322
Liz Favre <egfavre@gmail.com>
2016-05-17 11:58:09 -0400

    Add constructors and methods

--- src/Person.java
+++ src/Person.java
@@ -2,9 +2,30 @@
  * Created by user on 5/16/16.
  */
 public class Person {
+    static int familySize = 10;
     String name;
     int age;
     boolean isAlive;
     //create Person Array in Person class- self referntial
-    Person[] family = new Person[10];
+
+    Person[] family = new Person[familySize];
+
+    public Person(String name, int age, boolean isAlive){
+        this.name = name;
+        this.age = age;
+        this.isAlive = isAlive;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int newAge) {
+    }
+
+  /*  public boolean isValidName(String newName){
+        return newName.contains(" ");
+    }
+*/
 }
