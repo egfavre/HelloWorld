548fd69c38670fc0139b4135201b6a289d56884d
Liz Favre <egfavre@gmail.com>
2016-06-08 10:23:04 -0400

    Exercise 4 convert String[] to arraylist and put in hashmap

---
+++ src/Exercise04.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by user on 6/8/16.
+ */
+public class Exercise04 {
+
+
+    public static void main(String[] args) {
+        String[] names = {"Alice", "Smith", "Bob", "Jones", "Charlie", "Brown"};
+
+        //make Hashmap {"Smith": "Alice", etc}
+        HashMap<String, String> namesMap = new HashMap<>();
+
+        for (int i = 0; i <names.length; i+=2){
+            String firstName = names[i];
+            String lastName = names[i + 1];
+            namesMap.put(lastName, firstName);
+        }
+
+        System.out.println(names);
+        System.out.println(namesMap);
+
+        //solution 2
+        namesMap = new HashMap<>();
+        ArrayList<String> namesArr = new ArrayList<>(Arrays.asList(names));
+
+        ArrayList<String> pair;
+        while (namesArr.size() > 0) {
+            pair = new ArrayList<>(namesArr.subList(0,2));
+            namesArr = new ArrayList<>(namesArr.subList(2, namesArr.size()));
+            namesMap.put(pair.get(1), pair.get(0));
+        }
+        System.out.println(namesMap);
+    }
+}
