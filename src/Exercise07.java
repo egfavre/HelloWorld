a2a64bcb2a140bf2a8a3953fadf52bff0b26e0bd
Liz Favre <egfavre@gmail.com>
2016-06-28 11:04:09 -0400

    Exercise07 palindromes

---
+++ src/Exercise07.java
@@ -0,0 +1,35 @@
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
+/**
+ * Created by user on 6/28/16.
+ */
+public class Exercise07 {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("racecar"));
+        System.out.println(isPalindrome("hello"));
+
+    }
+
+    public static boolean isPalindrome(String input){
+
+        char[] ca = input.toCharArray();
+        char
+        for (char c:ca) {
+
+        }
+
+
+
+    }
+
+    //palindrome dates- SPARC interview
+    /*
+    date range: 01/01/2016 to 12/31/2016
+    go through each possible day and save the ones that are palindromes
+    get rid of slashes
+    MDDYY 61016
+    need date api, can't inc ints
+
+
+     */
+}
